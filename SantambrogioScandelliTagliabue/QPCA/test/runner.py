76ec7b2c089cb14604a64561984d0f040e31144e
unknown <riccardo.santambrogio@mail.polimi.it>
2020-05-27 11:03:59 +0200

    merge

--- SantambrogioScandelliTagliabue/QPCA/test/runner.py
+++ SantambrogioScandelliTagliabue/QPCA/test/runner.py
@@ -1,6 +1,5 @@
 import os
 
-
-for i in range(15,33,4):
-    os.system('python qpca_error_test.py 2 3 3 5 2 0.'+str(i)+' 200')
-
+for j in range(3):
+    for i in range(15,33,4):
+        os.system('python qpca_error_test.py 2 6 0 '+str(2**j)+' 2 0.'+str(i)+' 200')
