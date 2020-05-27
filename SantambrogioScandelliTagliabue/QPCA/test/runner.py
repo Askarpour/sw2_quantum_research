0eb1350487f200de6e1726b6dfe1d49acc9ec627
michele scandelli <miki199714@gmail.com>
2020-05-27 10:10:56 +0200

    add test and update viewer

--- SantambrogioScandelliTagliabue/QPCA/test/runner.py
+++ SantambrogioScandelliTagliabue/QPCA/test/runner.py
@@ -1,6 +1,7 @@
 import os
+import numpy as np
 
-
-for i in range(15,33,4):
-    os.system('python qpca_error_test.py 2 3 3 5 2 0.'+str(i)+' 200')
+for j in range(4,6):
+    for i in np.arange(0.05,0.15,0.02):
+        os.system('python qpca_error_test.py 2 '+str(j)+' 3 8 2 '+str(i)[:4]+' 200')
 
