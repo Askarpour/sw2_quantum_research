5e7fa31f0c8d4d3b649274098e3ed99103a54140
unknown <riccardo.santambrogio@mail.polimi.it>
2020-07-10 10:52:37 +0200

    add graphs pictures

--- SantambrogioScandelliTagliabue/QPCA/test/plot_mse.py
+++ SantambrogioScandelliTagliabue/QPCA/test/plot_mse.py
@@ -3,25 +3,23 @@ import matplotlib.pyplot as plt
 from masksfound import filemetrics_fix
 
 result = []
-DIM=16
+DIM=8
 NBITS=7
 ITERATIONS=2
 
 
-ra2 = [1,2,4,6,8,10]
-
-
+ra2 = [1,2,4,6,8]
 
 for i in range(1,11):
     if i in ra2:
-        result.append(filemetrics_fix(DIM,NBITS,2,i))
+        result.append(filemetrics_fix(DIM,NBITS,ITERATIONS,i))
 
-    
+
 print(result)
 
 plt.figure(0)
-plt.title("Mask/randvects "+str(DIM)+"x"+str(DIM))
-
+plt.title(str(DIM)+"x"+str(DIM)+", precision "+str(NBITS))
+plt.xlabel("initial vectors")
+plt.ylabel("top-k found")
 plt.plot( ra2, [i[1][0] for i in result])
-        
-
+plt.savefig(str(DIM)+"x"+str(DIM)+"_"+str(NBITS)+"_reduced.png")
